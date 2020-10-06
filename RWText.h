f0b3272efb2a8d802816bc033d5c9c2fab688920
lower0 <47355000+lower0@users.noreply.github.com>
2020-10-06 11:21:53 -0400

    Update RWText.h

--- RWText.h
+++ RWText.h
@@ -13,7 +13,7 @@ class PVL_RB_01_API URWText : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY() public:
 
-		UFUNCTION(BlueprintCallable, Category = "Custom", meta = (Keywords = "Save"))
+		UFUNCTION(BlueprintCallable, Category = "RW_Text", meta = (Keywords = "Save"))
 		static bool SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText, bool AllowOverWriting);
 	
 };
